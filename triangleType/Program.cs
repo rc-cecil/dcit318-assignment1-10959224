44d79c151ac2c2b972296acfbc9a2899843d237b
Robert Clottey <rcnkclottey001@st.ug.edu.gh>
2024-06-02 17:36:46 +0000

    triangle type identified

---
+++ triangleType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of first side : ");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of second side : ");
+            double side2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of third side : ");
+            double side3 = double.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+                Console.WriteLine("Equilateral triangle");
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+                Console.WriteLine("Isosceles triangle");
+            else
+                Console.WriteLine("Scalene triangle");
+        }
+    }
+}
