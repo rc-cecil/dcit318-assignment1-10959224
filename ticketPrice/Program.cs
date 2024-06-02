52f0e459f20d9e1a54b50c8a46e48cd6691ba6bd
Robert Clottey <rcnkclottey001@st.ug.edu.gh>
2024-06-02 17:31:17 +0000

    ticket price updated

---
+++ ticketPrice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            decimal ticketPrice;
+            if (age >= 65 || age <= 12)
+                ticketPrice = 7.0m;
+            else
+                ticketPrice = 10.0m;
+
+            Console.WriteLine($"Ticket price: GHC{ticketPrice}");
+        }
+    }
+}
