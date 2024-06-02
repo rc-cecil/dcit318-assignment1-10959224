9b4ae2328d59188cef2227d55761b82a5c5d0066
Robert Clottey <rcnkclottey001@st.ug.edu.gh>
2024-06-02 17:24:13 +0000

    calculator setup done

---
+++ gradeCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter marks (0-100): ");
+            int grade = int.Parse(Console.ReadLine());
+
+            char letterGrade;
+            if (grade >= 90)
+                letterGrade = 'A';
+            else if (grade >= 80)
+                letterGrade = 'B';
+            else if (grade >= 70)
+                letterGrade = 'C';
+            else if (grade >= 60)
+                letterGrade = 'D';
+            else
+                letterGrade = 'F';
+
+            Console.WriteLine($"Your grade is: {letterGrade}");
+        }
+    }
+}
